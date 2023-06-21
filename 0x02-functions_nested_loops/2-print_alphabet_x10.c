8e504f58656f7825ee0e4eff327e950bf02f0514
Chriseyop1 <eyopgabriel4real@gmail.com>
2023-06-20 19:40:01 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_alphabet - Prints the alphabet 10 times.
+*
+* Return: Alway 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+int i;
+int j; 
+
+for (j = 1; i <= 10; j++)
+{
+for (j = 97; j <= 122; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
+}
+
