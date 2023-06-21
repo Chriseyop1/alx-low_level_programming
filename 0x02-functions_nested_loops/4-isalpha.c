a7218a287236c704c7b2ca7a0f51f5cbf99eaaf4
Chriseyop1 <eyopgabriel4real@gmail.com>
2023-06-20 20:57:01 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * _isalpha - Shows 1 if the input is a
 * letter Another cases, shows 0
@@ -15,7 +16,7 @@ return (1);
 }
 else
 {
-retun (0);
+return (0);
 }
 _putchar('\n');
 }
