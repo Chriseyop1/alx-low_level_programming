6cb2f9d04e0bc77d27b943f917d82a92c0e35800
Chriseyop1 <eyopgabriel4real@gmail.com>
2023-06-21 09:14:39 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,45 @@
 #include "main.h"
-
 /**
-* add - Adds two values
-* @n1: The first number to be computed
-* @n2: The second number to be computed
-* Return: Integer value
+* add - adds two integers and returns the result
+* @a: int to be added to b
+* @b: int to be added to a
+* Return: sum of a and b
 */
-int add(int n1, int n2)
+int add(int a, int b)
 {
-return (n1 + n2);
+int c;
+c = a + b;
+return (c);
+}
+11 print_to_98.c
+#include <stdio.h>
+#include "main.h"
+/**
+* print_to_98 - prints all natural numbers from n to 98,
+* followed by a new line
+* @n: print from this number
+*/
+void print_to_98(int n)
+{
+int i, j;
+if (n <= 98)
+{
+for (i = n; i <= 98; i++)
+{
+if (i != 98)
+printf("%d, ", i);
+else if (i == 98)
+printf("%d\n", i);
+}
+}
+else if (n >= 98)
+{
+for (j = n; j >= 98; j--)
+{
+if (j != 98)
+printf("%d, ", j);
+else if (j == 98)
+printf("%d\n", j);
+}
+}
 }
