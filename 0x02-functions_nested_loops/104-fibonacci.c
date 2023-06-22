ed1049c9bdd5c4ed15a374dde0f98ad4b3fd6ca2
Chriseyop1 <eyopgabriel4real@gmail.com>
2023-06-22 05:34:48 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -20,7 +20,7 @@ printf("%lu", bef);
 for (i = 1; i < 91; i++)
 {
 printf(", %lu", aft);
-aft += bef; 
+aft += bef;
 bef = aft - bef;
 }
 bef1 = (bef / l);
@@ -31,10 +31,10 @@ for (i = 92; i < 99; ++i)
 {
 printf(", %lu", aft1 + (aft2 / l));
 printf("%lu", aft2 % l);
-aft1 = aft1 + bef1; 
-bef1 = aft1 - bef1; 
-aft2 = aft2 + bef2; 
-bef2 = aft2 - bef2; 
+aft1 = aft1 + bef1;
+bef1 = aft1 - bef1;
+aft2 = aft2 + bef2;
+bef2 = aft2 - bef2;
 }
 printf("\n");
 return (0);
